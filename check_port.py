e27426b50cee216ec0308cc190793a0bf7a710e3
ocean-onion <oisin.harrison31@gmail.com>
2024-08-07 22:28:05 +0800

    Update check_port.py

--- check_port.py
+++ check_port.py
@@ -1,13 +1,16 @@
 import socket
 
-def check_port(port):
+def check_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(('localhost', port)) == 0
+        result = s.connect_ex(('localhost', port))
+        if result == 0:
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
-    port = 5001
-    if check_port(port):
-        print(f"Port {port} is in use")
+    port = 5000
+    if check_port_in_use(port):
+        print(f"Port {port} is in use.")
     else:
-        print(f"Port {port} is free")
-
+        print(f"Port {port} is available.")
