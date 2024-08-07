55329fa438a9f1877da1d1dbfa94f419943904e3
ocean-onion <oisin.harrison31@gmail.com>
2024-08-07 23:05:35 +0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from app.models import User
 
 class RegistrationForm(FlaskForm):
+    """Form for users to create new account"""
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -11,29 +12,35 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
+        """Username must be unique"""
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
+        """Email must be unique"""
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
+    """Form for users to login"""
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
 class PluginUploadForm(FlaskForm):
+    """Form for users to upload a plugin file"""
     plugin_file = FileField('Plugin File', validators=[DataRequired()])
     submit = SubmitField('Upload')
 
 class CustomizationForm(FlaskForm):
+    """Form for users to input customization options for plugins/mods"""
     customization_options = TextAreaField('Customization Options', validators=[DataRequired()])
     submit = SubmitField('Customize')
 
 class RatingForm(FlaskForm):
+    """Form for users to rate a plugin or mod"""
     rating = StringField('Rating', validators=[DataRequired()])
     submit = SubmitField('Rate')
